7b8b33d62e1056a94d733ceac828727835c941e5
Emmanuel Odii <emmanuelodii80@gmail.com>
2026-06-27 12:55:05 +0100

    fix: * restarted migrations

--- apps/web/db/migrations/0001_elite_ultimates.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "app" RENAME COLUMN "manifest" TO "scopes";--> statement-breakpoint
-ALTER TABLE "app" ADD COLUMN "sensitiveKeys" text[];--> statement-breakpoint
-ALTER TABLE "app" ADD COLUMN "version" text NOT NULL;
